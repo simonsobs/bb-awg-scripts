4dfccaa75d6d135e81eb9433c2b0a0cb5ccae012
Kevin Wolz <kevin.wolz93@gmail.com>
2025-01-24 14:49:59 -0800

    multilayer compatibility for filtering sims

--- pipeline/filtering/filter_sims_sotodlib.py
+++ pipeline/filtering/filter_sims_sotodlib.py
@@ -4,17 +4,15 @@ import argparse
 import logging
 import sqlite3
 import os
-import yaml
 import sys
 import time
 import sotodlib.mapmaking.demod_mapmaker as dmm
+import sotodlib.preprocess.preprocess_util as pp_util
 
 from sotodlib.mapmaking.noise_model import NmatUnit
-from sotodlib.core import Context
 from sotodlib.site_pipeline import preprocess_tod
 from sotodlib.core.metadata import loader
-from sotodlib.coords import demod
-from pixell import enmap, utils
+from pixell import enmap
 from mpi4py import MPI
 
 sys.path.append("/global/homes/k/kwolz/bbdev/bb-awg-scripts/pipeline/bundling")
@@ -65,6 +63,57 @@ def erik_make_map(obs, shape=None, wcs=None, nside=None, site=None):
     return wmap, weights
 
 
+def make_map(obs, pix_type="hp", shape=None, wcs=None, nside=None, site=None,
+             logger=None):
+    """
+    Run the healpix mapmaker (currently in sotodlib sat-mapmaking-er-dev) on a
+    given observation.
+
+    Parameters
+    ----------
+        obs: axis manager
+            Input TOD, after demodulation.
+        shape: numpy.ndarray
+            Shape of the output map geometry (CAR only)
+        wcs: wcs
+            WCS of the output map geometry (CAR only)
+        nside: int
+            Nside of the output map (HEALPix only)
+        site: str
+            Site of the observation, e.g. `so_sat1`
+    Returns
+    -------
+        wmap: numpy.ndarray
+            Output weighted TQU map, in healpix NESTED scheme
+        weights: numpy.ndarray
+            Output TQU weights, in healpix NESTED scheme
+    """
+    obs.wrap("weather", np.full(1, "toco"))
+    obs.wrap("site", np.full(1, site))
+    if not hasattr(obs.flags, "glitch_flags"):
+        obs.flags.wrap(
+            'glitch_flags',
+            (obs.preprocess.turnaround_flags.turnarounds
+             + obs.preprocess.jumps_2pi.jump_flag
+             + obs.preprocess.glitches.glitch_flags)
+        )
+    if pix_type == "car":
+        nside = None
+        assert (shape is not None) and (wcs is not None)
+    elif pix_type == "hp":
+        wcs, shape = (None, None)
+        assert nside is not None
+    mapmaker = dmm.setup_demod_map(
+        NmatUnit(), shape=shape, wcs=wcs, nside=nside, split_labels=["science"]
+    )
+    mapmaker.add_obs('signal', obs)
+    wmap = mapmaker.signals[0].rhs[0]
+    weights = np.diagonal(mapmaker.signals[0].div[0], axis1=0, axis2=1)
+    weights = np.moveaxis(weights, -1, 0)
+
+    return wmap, weights
+
+
 def get_logger(fmt=None, datefmt=None, debug=False, **kwargs):
     """Return logger from logging module
     code from pspipe
@@ -102,9 +151,11 @@ def main(args):
     size = comm.Get_size()
     rank = comm.Get_rank()
 
+    logger = pp_util.init_logger("benchmark", verbosity=3)
+
     # ArgumentParser
     out_dir = args.output_directory
-    print("out_dir", out_dir)
+    logger.info(f"out_dir: {out_dir}")
     os.makedirs(out_dir, exist_ok=True)
 
     plot_dir = f"{out_dir}/plots"
@@ -119,6 +170,13 @@ def main(args):
 
     # Config files
     preprocess_config = args.preprocess_config
+    preprocess_config_init = args.preprocess_config_init
+    preprocess_config_proc = args.preprocess_config_proc
+    do_multilayer_preproc = (preprocess_config_init is not None
+                             and preprocess_config_proc is not None)
+
+    logger.debug(f"atomic DB: {atom_db}")
+    logger.debug(f"do_multilayer_preproc? {do_multilayer_preproc}")
 
     # Sim related arguments
     map_dir = args.map_dir
@@ -137,7 +195,7 @@ def main(args):
 
     if pix_type == "car" and car_map_template is not None:
         w = enmap.read_map(car_map_template)
-        # shape = w.shape[-2:]
+        shape = w.shape[-2:]
         wcs = w.wcs
 
     # Bundle query arguments
@@ -148,7 +206,7 @@ def main(args):
     # Extract list of ctimes from bundle database for the given
     # bundle_id - null split combination
     if os.path.isfile(bundle_db):
-        print(f"Loading from {bundle_db}.")
+        logger.info(f"Loading from {bundle_db}.")
         bundle_coordinator = BundleCoordinator.from_dbfile(
             bundle_db, bundle_id=bundle_id,
             null_prop_val=null_prop_val_inter_obs
@@ -172,29 +230,36 @@ def main(args):
     atomic_metadata = []
     db_con = sqlite3.connect(atom_db)
     db_cur = db_con.cursor()
+
     for ctime in ctimes:
         res = db_cur.execute(
             "SELECT obs_id, wafer FROM atomic WHERE "
-            f"freq_channel == '{freq_channel}' AND ctime == '{ctime}'"
+            f"freq_channel == '{freq_channel}' AND ctime = '{ctime}'"
         )
         res = res.fetchall()
+
         for obs_id, wafer in res:
             atom_id = (obs_id, wafer, freq_channel)
-            restrict = (args.atomic_list is not None
-                        and (atom_id not in atomic_restrict))
-            if not restrict and atom_id not in atomic_metadata:
+
+            do_include = (args.atomic_list is None
+                          or (atom_id in atomic_restrict))
+            if do_include and not (atom_id in atomic_metadata):
                 atomic_metadata.append((obs_id, wafer, freq_channel))
     db_con.close()
 
-    print(
-        f"{len(atomic_metadata)} atomic file names (bundle {bundle_id})"
-    )
+    logger.info(f"Using {len(atomic_metadata)} atomic maps to filter sims.")
 
     # Load preprocessing pipeline and extract from it list of preprocessing
     # metadata (detectors, samples, etc.) corresponding to each atomic map
-    config = yaml.safe_load(open(preprocess_config, "r"))
-    context = config["context_file"]
-    ctx = Context(context)
+    if do_multilayer_preproc:
+        configs_init, ctx_init = pp_util.get_preprocess_context(
+            preprocess_config_init
+        )
+        configs_proc, ctx_proc = pp_util.get_preprocess_context(
+            preprocess_config_proc
+        )
+    else:
+        configs, ctx = pp_util.get_preprocess_context(preprocess_config)
 
     # Distribute [natomics x nsims] tasks among [size] workers
     if "," in sim_ids:
@@ -207,9 +272,8 @@ def main(args):
 
     ids = np.arange(id_min, id_max+1)
     mpi_shared_list = [(i, j) for i in ids for j in atomic_metadata]
-
-    log = get_logger()
-    task_ids = distribute_tasks(size, rank, len(mpi_shared_list), logger=log)
+    task_ids = distribute_tasks(size, rank, len(mpi_shared_list),
+                                logger=logger)
     local_mpi_list = [mpi_shared_list[i] for i in task_ids]
 
     # Loop over local tasks (sim_id, atomic_id). For each of these, do:
@@ -226,19 +290,19 @@ def main(args):
         map_file = f"{map_dir}/{map_fname}"
 
         if args.pix_type == "car":
-            print("Loading CAR")
+            logger.info(f"Loading CAR map: {map_file}")
             sim = enmap.read_map(map_file)
-            print("sim.geometry", sim.geometry)
         elif args.pix_type == "hp":
-            print("Loading HEALPix")
+            logger.info(f"Loading HP map: {map_file}")
             sim = hp.read_map(map_file, field=[0, 1, 2])
         else:
             raise ValueError("pix_type must be hp or car")
 
-        log.info(f"***** Doing {obs_id} {wafer} {freq} "
-                 f"and SIMULATION {sim_id} *****")
+        logger.info(f"Loading {obs_id} {wafer} {freq} on sim {sim_id}")
         dets = {"wafer_slot": wafer, "wafer.bandpass": freq}
-        meta = ctx.get_meta(obs_id, dets=dets)
+
+        ctx = ctx_proc if do_multilayer_preproc else ctx
+        meta = ctx_proc.get_meta(obs_id=obs_id, dets=dets)
 
         # Focal plane thinning
         if args.fp_thin is not None:
@@ -248,48 +312,64 @@ def main(args):
             meta.restrict("dets", thinned)
 
         # Missing pointing not cut in preprocessing
-        meta.restrict(
-            "dets", meta.dets.vals[~np.isnan(meta.focal_plane.gamma)]
-        )
-        # FIXME: Adapt this function to the 2-stage preprocessing in sotodlib.
+        # meta.restrict(
+        #     "dets", meta.dets.vals[~np.isnan(meta.focal_plane.gamma)]
+        # )
         try:
-            aman = preprocess_tod.load_preprocess_tod_sim(
-                obs_id,
-                sim_map=sim,
-                configs=config,
-                meta=meta,
-                modulated=True,
-                # site="so_sat1",  # new field required from new from_map()
-                # ordering="RING"  # new field required for healpix
-            )
-            log.info(f"Loaded {obs_id}, {wafer}, {freq}")
+            if do_multilayer_preproc:
+                aman = pp_util.multilayer_load_and_preprocess_sim(
+                    obs_id,
+                    configs_init=configs_init,
+                    configs_proc=configs_proc,
+                    sim_map=sim,
+                    meta=meta,
+                    logger=logger
+                )
+            else:
+                aman = preprocess_tod.load_preprocess_tod_sim(
+                    obs_id,
+                    sim_map=sim,
+                    configs=configs,
+                    meta=meta,
+                    modulated=True,
+                    logger=logger,
+                    dets=dets,
+                    # site="so_sat1",  # new field required from new from_map()
+                    # ordering="RING"  # new field required for healpix
+                )
         except loader.LoaderError:
-            print(f"ERROR: {obs_id} {wafer} {freq} metadata is not there. "
-                  "SKIPPING.")
+            logger.info(
+                f"ERROR: {obs_id} {wafer} {freq} metadata is not there. "
+                "SKIPPING."
+            )
             continue
 
         if aman.dets.count <= 1:
             continue
 
-        # FIXME: Unify the mapmaking function to accomodate hp or car.
-        if pix_type == "car":
-            filtered_sim = demod.make_map(
-                aman,
-                res=10*utils.arcmin,
-                wcs_kernel=wcs,
-            )
-            wmap, w = filtered_sim["weighted_map"], filtered_sim["weight"]
-            w = np.moveaxis(w.diagonal(), -1, 0)
+        # # OLD CODE
+        # if pix_type == "car":
+        #     filtered_sim = demod.make_map(
+        #         aman,
+        #         res=10*utils.arcmin,
+        #         wcs_kernel=wcs,
+        #     )
+        #     wmap, w = filtered_sim["weighted_map"], filtered_sim["weight"]
+        #     w = np.moveaxis(w.diagonal(), -1, 0)
 
-        elif pix_type == "hp":
-            wmap, w = erik_make_map(aman, nside=nside, site="so_sat1")
+        # elif pix_type == "hp":
+        #     wmap, w = erik_make_map(aman, nside=nside, site="so_sat1")
+
+        # NEW CODE
+        wmap, w = make_map(aman, pix_type, shape=shape, wcs=wcs, nside=nside,
+                           logger=logger)
 
         local_wmaps.append(wmap)
         local_weights.append(w)
         local_labels.append(sim_id)
 
         # Saving filtered atomics to disk
-        log.info(f"Rank {rank} saving labels {local_labels}")
+        logger.info(f"Rank {rank} saving labels {local_labels}")
         atomic_fname = map_string_format.format(sim_id=sim_id).replace(
             mfmt,
             f"_obsid{obs_id}_{wafer}_{freq_channel}{mfmt}"
@@ -308,9 +388,8 @@ def main(args):
             hp.write_map(
                 f_w, w, dtype=np.float32, overwrite=True, nest=True
             )
-
         end = time.time()
-        print(f"*** ELAPSED TIME for filtering: {end - start} seconds. ***")
+        logger.info(f"ELAPSED TIME for filtering: {end - start} seconds.")
 
 
 if __name__ == "__main__":
@@ -331,7 +410,18 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--preprocess-config",
-        help="Path to the preprocessing config file."
+        help="Path to the preprocessing config file.",
+        default=None
+    )
+    parser.add_argument(
+        "--preprocess-config-init",
+        help="Path to the init preprocessing config (multilayer) file.",
+        default=None
+    )
+    parser.add_argument(
+        "--preprocess-config-proc",
+        help="Path to the proc preprocessing config (multilayer) file.",
+        default=None
     )
     parser.add_argument(
         "--map-dir",
