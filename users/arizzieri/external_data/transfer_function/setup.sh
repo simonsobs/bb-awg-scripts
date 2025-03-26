d407e1557be47e41e520c2f726100cac37df33ea
Arianna Rizzieri <rizzieri@apc.in2p3.fr>
2025-03-26 03:43:11 -0700

    first tests on external data

---
+++ users/arizzieri/external_data/transfer_function/setup.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# soopercool_dir=/global/homes/a/arianna/SO/SOOPERCOOL
+bb_awg_scripts_dir=/global/homes/a/arianna/SO/bb-awg-scripts
+pwg_scripts_dir=/global/homes/a/arianna/SO/pwg-scripts
+
+# soopercool_config=/global/homes/a/arianna/SO/pwg-scripts/iso-sat-review/soopercool_configs/satp1/paramfile_satp1_south_scan_left_right.yaml
+filtering_config=${bb_awg_scripts_dir}/users/arizzieri/external_data/transfer_function/satp3/filtering_config_satp3.yaml
