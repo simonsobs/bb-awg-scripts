232ac4b2495da9cc6e68bf90a6966369c5631c96
Arianna Rizzieri <ar3186@tiger3.princeton.edu>
2025-04-02 11:20:17 -0400

    added tiger paths

--- users/arizzieri/external_data/transfer_function/setup.sh
+++ users/arizzieri/external_data/transfer_function/setup.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
-# soopercool_dir=/global/homes/a/arianna/SO/SOOPERCOOL
-bb_awg_scripts_dir=/global/homes/a/arianna/SO/bb-awg-scripts
-pwg_scripts_dir=/global/homes/a/arianna/SO/pwg-scripts
+# BASE_DIR="/global/homes/a/arianna/SO"  ##@nersc
+BASE_DIR="/home/ar3186"                ##@tiger3
 
-# soopercool_config=/global/homes/a/arianna/SO/pwg-scripts/iso-sat-review/soopercool_configs/satp1/paramfile_satp1_south_scan_left_right.yaml
-filtering_config=${bb_awg_scripts_dir}/users/arizzieri/external_data/transfer_function/satp3/filtering_config_satp3.yaml
+# soopercool_dir=/global/homes/a/arianna/SO/SOOPERCOOL
+bb_awg_scripts_dir="$BASE_DIR/bb-awg-scripts"
+pwg_scripts_dir="$BASE_DIR/pwg-scripts"
+
+# soopercool_config="$BASE_DIR/pwg-scripts/iso-sat-review/soopercool_configs/satp1/paramfile_satp1_south_scan_left_right.yaml"
+filtering_config="$bb_awg_scripts_dir/users/arizzieri/external_data/transfer_function/satp3/filtering_config_satp3.yaml"
