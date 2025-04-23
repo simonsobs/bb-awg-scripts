c305a2f01b995ae8e48ebc1cb92bafba4dbb523f
Arianna Rizzieri <ar3186@tiger3.princeton.edu>
2025-04-23 13:59:32 -0400

    files for ext data runs with planck split A and B

--- users/arizzieri/external_data/transfer_function/setup.sh
+++ users/arizzieri/external_data/transfer_function/setup.sh
@@ -7,5 +7,5 @@ bb_awg_scripts_dir="$BASE_DIR/bb-awg-scripts"
 pwg_scripts_dir="$BASE_DIR/pwg-scripts"
 soopercool_dir="$BASE_DIR/SOOPERCOOL"
 
-filtering_config="$bb_awg_scripts_dir/users/arizzieri/external_data/transfer_function/satp3/filtering_config_satp3.yaml"
+filtering_config="$bb_awg_scripts_dir/users/arizzieri/external_data/transfer_function/satp3/filtering_config_satp3_planck_npipeA.yaml"
 soopercool_config="$bb_awg_scripts_dir/users/arizzieri/external_data/transfer_function/satp3/paramfile_satp3_south_science.yaml"
