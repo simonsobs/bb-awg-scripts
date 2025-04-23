ba38a7d6a4f52b375f80cc24aacafc5d539ffa79
Arianna Rizzieri <ar3186@tiger3.princeton.edu>
2025-04-23 10:32:17 -0400

    first external data test run

--- users/arizzieri/external_data/transfer_function/filter_tf_ext.sh
+++ users/arizzieri/external_data/transfer_function/filter_tf_ext.sh
@@ -2,6 +2,6 @@
 
 . setup.sh
 
-# srun -n 112 -c 4 --cpu_bind=cores \
+srun -n 112 -c 4 --cpu_bind=cores \
 python ${bb_awg_scripts_dir}/pipeline/filtering/filter_ext_sotodlib.py \
     --config_file $filtering_config
